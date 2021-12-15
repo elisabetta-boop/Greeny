1d3bbd43c5cb9b9ebfb653ab5f06936ae44b796e
Elisabetta Saporetti <elisabetta.saporetti@yahoo.it>
2021-12-15 12:34:11 +0100

    ok dialog box

--- Assets/#Project/Script/TextWriter.cs
+++ Assets/#Project/Script/TextWriter.cs
@@ -1,48 +1,165 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TextWriter : MonoBehaviour
-{
+public class TextWriter : MonoBehaviour {
+
+private static TextWriter instance;
+
+private List<TextWriterSingle> textWriterSingleList;
+
+private void Awake() {
+    instance = this;
+    textWriterSingleList = new List<TextWriterSingle>();
+}
+
+public static TextWriterSingle AddWriter_Static(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters, bool removeWriterBeforeAdd, Action onComplete) {
+    if (removeWriterBeforeAdd) {
+        instance.RemoveWriter(uiText);
+    }
+    return instance.AddWriter(uiText, textToWrite, timePerCharacter, invisibleCharacters, onComplete);
+}
+
+private TextWriterSingle AddWriter(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters, Action onComplete) {
+    TextWriterSingle textWriterSingle = new TextWriterSingle(uiText, textToWrite, timePerCharacter, invisibleCharacters, onComplete);
+    textWriterSingleList.Add(textWriterSingle);
+    return textWriterSingle;
+}
+
+public static void RemoveWriter_Static(Text uiText) {
+    instance.RemoveWriter(uiText);
+}
+
+private void RemoveWriter(Text uiText) {
+    for (int i = 0; i < textWriterSingleList.Count; i++) {
+        if (textWriterSingleList[i].GetUIText() == uiText) {
+            textWriterSingleList.RemoveAt(i);
+            i--;
+        }
+    }
+}
+
+private void Update() {
+    for (int i = 0; i < textWriterSingleList.Count; i++) {
+        bool destroyInstance = textWriterSingleList[i].Update();
+        if (destroyInstance) {
+            textWriterSingleList.RemoveAt(i);
+            i--;
+        }
+    }
+}
+
+/*
+    * Represents a single TextWriter instance
+    * */
+public class TextWriterSingle {
+
     private Text uiText;
     private string textToWrite;
+    private int characterIndex;
     private float timePerCharacter;
     private float timer;
-    private int CharacterIndex;
     private bool invisibleCharacters;
-    public void AddWriter(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
-    {
+    private Action onComplete;
+
+    public TextWriterSingle(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters, Action onComplete) {
         this.uiText = uiText;
         this.textToWrite = textToWrite;
         this.timePerCharacter = timePerCharacter;
         this.invisibleCharacters = invisibleCharacters;
-        CharacterIndex=0;
+        this.onComplete = onComplete;
+        characterIndex = 0;
     }
-    private void Update()
-    {
-        if (uiText != null)
-        {
-            timer -= Time.deltaTime;
-            while(timer <= 0)
-            {
-                //Display next character
-                timer += timePerCharacter;
-                CharacterIndex++;
-                string text = textToWrite.Substring(0,CharacterIndex);
-                if(invisibleCharacters)
-                {
-                    text += "<color=#00000000>"+ textToWrite.Substring(CharacterIndex)+ "</color>";
-                }
-                uiText.text = text;
 
-                if(CharacterIndex >= textToWrite.Length)
-                {
-                    uiText = null;
-                    return;
-                }
+    // Returns true on complete
+    public bool Update() {
+        timer -= Time.deltaTime;
+        while (timer <= 0f) {
+            // Display next character
+            timer += timePerCharacter;
+            characterIndex++;
+            string text = textToWrite.Substring(0, characterIndex);
+            if (invisibleCharacters) {
+                text += "<color=#00000000>" + textToWrite.Substring(characterIndex) + "</color>";
+            }
+            uiText.text = text;
+
+            if (characterIndex >= textToWrite.Length) {
+                // Entire string displayed
+                if (onComplete != null) onComplete();
+                return true;
             }
         }
+
+        return false;
     }
+
+    public Text GetUIText() {
+        return uiText;
+    }
+
+    public bool IsActive() {
+        return characterIndex < textToWrite.Length;
+    }
+
+    public void WriteAllAndDestroy() {
+        uiText.text = textToWrite;
+        characterIndex = textToWrite.Length;
+        if (onComplete != null) onComplete();
+        TextWriter.RemoveWriter_Static(uiText);
+    }
+
+
 }
+
+
+}// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class TextWriter : MonoBehaviour
+// {
+//     private Text uiText;
+//     private string textToWrite;
+//     private float timePerCharacter;
+//     private float timer;
+//     private int CharacterIndex;
+//     private bool invisibleCharacters;
+//     public void AddWriter(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+//     {
+//         this.uiText = uiText;
+//         this.textToWrite = textToWrite;
+//         this.timePerCharacter = timePerCharacter;
+//         this.invisibleCharacters = invisibleCharacters;
+//         CharacterIndex=0;
+//     }
+//     private void Update()
+//     {
+//         if (uiText != null)
+//         {
+//             timer -= Time.deltaTime;
+//             while(timer <= 0)
+//             {
+//                 //Display next character
+//                 timer += timePerCharacter;
+//                 CharacterIndex++;
+//                 string text = textToWrite.Substring(0,CharacterIndex);
+//                 if(invisibleCharacters)
+//                 {
+//                     text += "<color=#00000000>"+ textToWrite.Substring(CharacterIndex)+ "</color>";
+//                 }
+//                 uiText.text = text;
+
+//                 if(CharacterIndex >= textToWrite.Length)
+//                 {
+//                     uiText = null;
+//                     return;
+//                 }
+//             }
+//         }
+//     }
+// }
 
--- Assets/#Project/Script/UI_Assistant.cs
+++ Assets/#Project/Script/UI_Assistant.cs
@@ -1,27 +1,82 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class UI_Assistant : MonoBehaviour
-{
-    [SerializeField]
-    private TextWriter textWriter;
+using CodeMonkey.Utils;
+
+public class UI_Assistant : MonoBehaviour {
+
     private Text messageText;
-    private void Awake()
-    {
-        messageText = transform.Find("Messages").Find("messageText").GetComponentInChildren<Text>();
-        Application.targetFrameRate = 3;
-    }
-    void Start()
-    {
-        messageText.text ="Hello Wolrd";
-        textWriter.AddWriter(messageText,"Hello World", .1f,true);
+    private TextWriter.TextWriterSingle textWriterSingle;
+    private AudioSource talkingAudioSource;
+    private Animator animator;
+
+    private void Awake() {
+        messageText = transform.Find("Messages").Find("messageText").GetComponent<Text>();
+        talkingAudioSource = transform.Find("TalkingSound").GetComponent<AudioSource>();
+        animator = GetComponentInChildren<Animator>();
+
+        transform.Find("Messages").GetComponent<Button_UI>().ClickFunc = () => {
+            if (textWriterSingle != null && textWriterSingle.IsActive()) {
+                // Currently active TextWriter
+                textWriterSingle.WriteAllAndDestroy();
+            } else {
+                string[] messageArray = new string[] {
+                    "This is the assistant speaking, hello and goodbye, see you next time!",
+                    "Hey there!",
+                    "This is a really cool and useful effect",
+                    "Let's learn some code and make awesome games!",
+                    "Check out Battle Royale Tycoon on Steam!",
+                };
+
+                string message = messageArray[Random.Range(0, messageArray.Length)];
+                StartTalkingSound();
+                textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .02f, true, true, StopTalkingSound);
+            }
+        };
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void StartTalkingSound() {
+        talkingAudioSource.Play();
+        animator.SetBool("isTalking", true);
     }
-    
+
+    private void StopTalkingSound() {
+        talkingAudioSource.Stop();
+        animator.SetBool("isTalking", false);
+    }
+
+    private void Start() {
+        //TextWriter.AddWriter_Static(messageText, "This is the assistant speaking, hello and goodbye, see you next time!", .1f, true);
+    }
+
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// public class UI_Assistant : MonoBehaviour
+// {
+//     [SerializeField]
+//     private TextWriter textWriter;
+//     private Text messageText;
+//     private void Awake()
+//     {
+//         messageText = transform.Find("Messages").Find("messageText").GetComponentInChildren<Text>();
+//         Application.targetFrameRate = 3;
+//     }
+//     void Start()
+//     {
+//         messageText.text ="Hello Wolrd";
+//         textWriter.AddWriter(messageText,"Hello World", .1f,true);
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+    
+// }
